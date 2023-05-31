7bc1f8e2daee6753ca7b57e57ea657cb88ce38d2
pRaNaY <www.pranay1337.com@gmail.com>
2023-05-31 16:44:32 +0530

    Added theme and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:beamer/beamer.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:spotify_flutter/beamer_config.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 import 'main_navigation.dart';
 
@@ -28,7 +31,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      theme: FlexThemeData.dark(scheme: FlexScheme.blueM3,appBarElevation:0.7,fontFamily: 
+      GoogleFonts.rubik().fontFamily,
+              // We use the nicer Material 3 Typography in both M2 and M3 mode.
+              typography: Typography.material2021(
+                platform: defaultTargetPlatform,
+              ) ),
+      // The Mandy red, dark theme.
+      darkTheme: FlexThemeData.dark(scheme: FlexScheme.blueM3,appBarElevation:0.7,
+      fontFamily: GoogleFonts.rubik().fontFamily,
+              // We use the nicer Material 3 Typography in both M2 and M3 mode.
+              typography: Typography.material2021(
+                platform: defaultTargetPlatform,
+              ) ),
+      // Use dark or light theme based on system setting.
+      themeMode: ThemeMode.system,
       routerDelegate: routerDelegate,
       routeInformationParser: BeamerParser(),
       backButtonDispatcher: BeamerBackButtonDispatcher(
