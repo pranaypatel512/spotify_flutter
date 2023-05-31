c879a92a590014880af3a0a6e1cda59672ed5f11
pRaNaY <www.pranay1337.com@gmail.com>
2023-05-31 17:56:05 +0530

    Added required network constants

---
+++ lib/core/constants/network_constants.dart
@@ -0,0 +1,11 @@
+class NetworkConstants {
+  // base url
+  static const String baseUrl = "https://api.spotify.com/v1/";
+  static const String baseUrlAuth = "https://accounts.spotify.com/api/token";
+
+  // receiveTimeout
+  static const int receiveTimeout = 15000;
+
+  // connectTimeout
+  static const int connectionTimeout = 30000;
+}
---
+++ lib/core/netowork/Interceptor/dio_interceptor.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class DioInterceptor extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    log("Request[${options.method}] => PATH: ${options.path}");
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    log('⬅️ Received network response');
+    log('${response.statusCode != 200 ? '❌ ${response.statusCode} ❌' : '✅ 200 ✅'} ${response.requestOptions.baseUrl}${response.requestOptions.path}');
+    log('Query params: ${response.requestOptions.queryParameters}');
+    log('-------------------------');
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    log("Error[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}");
+    super.onError(err, handler);
+  }
+}
