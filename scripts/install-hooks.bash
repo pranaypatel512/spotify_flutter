5f04c091c0f57c42fe6c859ef0c94bf7db0176f6
pRaNaY <www.pranay1337.com@gmail.com>
2023-06-19 14:27:41 +0530

    remove pre commit hook

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-GIT_DIR=$(git rev-parse --git-dir)
+#GIT_DIR=$(git rev-parse --git-dir)
 
-echo "Installing hooks..."
+#echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
-ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
-echo "Done!"
+#ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
+#echo "Done!"
